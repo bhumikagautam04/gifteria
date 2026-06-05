8ec4de70c44a0c9435b4583287ac71115d04cc9d
bhumikagautam04 <bhumikagautam751@gmail.com>
2026-06-05 14:21:51 +0530

    data fetch

--- public/javascripts/gift_data.js
+++ public/javascripts/gift_data.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-  $.get("/product/fetch_all_category", function(response){
+  $.getJSON("/product/fetch_all_category", function(response){
     //  alert(JSON.stringify(response))
     // Handle the response
     response.data.map((item)=>{
         $("#categoryid").append($("<option>").text(item.categoryname).val(item.categoryid))
     })
+     })
+  $("#categoryid").change(function(){
+    // alert("Changed");
+     $("#subcategoryid").empty()
+     $("#subcategoryid").append($("<option>").text("-Select Subcategory-"))
+    $.getJSON("/product/fetch_all_subcategory_by_categoryid",{categoryid: $("#categoryid").val()}, function(response){
+      // alert(JSON.stringify(response));
+      response.data.map((item)=>{
+        // alert(JSON.stringify(item));
+        $("#subcategoryid").append($("<option>").text(item.subcategoryname).val(item.subcategoryid))
+      })
+    })
   })
-})
+  })
--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,10 @@ var pool=require('./pool');
 var router = express.Router();
 
 router.get('/product_interface', function(req,res,next){
-    res.render('product_interface');
+    res.render('product_interface', {message:''});
+})
+router.get('/Display_all_products', function(req,res,next){
+    res.render('DisplayALLProducts');
 })
 
 router.get('/fetch_all_category', function(req,res){
@@ -17,9 +20,54 @@ router.get('/fetch_all_category', function(req,res){
         {
             res.json({status:true, data:result});
         }
+        })
+    })
+        
+   
+    router.get('/fetch_all_subcategory_by_categoryid', function(req,res){
+
+    pool.query("select * from subcategory where categoryid=?", [req.query.categoryid], function(err,result){
+        if(err)
+        {
+            res.json({status:false, message:err});
+        }
+        else
+        {
+            res.json({status:true, data:result});
+        }
+    })
     })
 
-}
-)
+     router.post('/submit_product', function(req,res){
+
+    pool.query("insert into products (categoryid, subcategoryid, productname, productrate, productoffer, stock, weight) values( ?, ?, ?, ?, ?, ?, ?)", [ req.body.categoryid, req.body.subcategoryid, req.body.productname, req.body.productrate, req.body.productoffer, req.body.productstock, req.body.productweight], function(err,result){
+        if(err)
+        {
+            res.render('product_interface', { message:"sql Error"});
+        }
+        else
+        {
+            res.render('product_interface', { message:"Rcord Submitteed Successfully"});
+        }
+    })
+    })
+    router.get('/fetch_all_products', function(req,res){
+
+    pool.query("select* from products", function(err,result){
+        if(err)
+        {
+            res.json({status:false, message:err});
+        }
+        else
+        {
+            res.json({status:true, data:result});
+        }
+        })
+    })
+ 
+
+ 
+
+
 
 module.exports = router;
